ede7d49c23480525581937c02faf9f51fd17574f
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-08-02 18:19:59 -0500

    tachyons update

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Header extends React.Component {
 	render(){
 		return(
-			<h1 className="">To-Do</h1>
+			<h1 className="f1 b courier bg-light-blue">To-Do</h1>
 			)
 	}
 }
